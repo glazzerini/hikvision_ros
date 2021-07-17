e8340df7fc08ac8490da81034d9e8ee042731e1b
nadir-kapetanovic <nadir.kapetanovic@fer.hr>
2021-07-17 10:41:54 +0200

    Modified .sh script for running hik_ptz node for losinj with appropriate env vars. Modified camera_name in bttm_side_du.launch so that the streams from all vehicles don't clash.

--- scripts/losinj_hik_setup_and_run.sh
+++ scripts/losinj_hik_setup_and_run.sh
@@ -2,7 +2,7 @@
 # Losinj IP=10.0.15.50
 
 export ASV_NAME=losinj
-export TOPCAM_IP=10.0.15.52
+export PLADYPOS_HIKVISION_IP=10.0.15.52
 export PLADYPOS_HIKVISION_STREAM=rtsp://stdops:labos123@$PLADYPOS_HIKVISION_IP:554
 
 echo 'Topside hostname: '$HOSTNAME 
@@ -10,14 +10,14 @@ echo 'Topside ROS distro:'$ROS_DISTRO
 export ROS_MASTER_URI=http://$ASV_NAME:11311/
 echo 'ROS master exported to: '$ROS_MASTER_URI
 #export ROS_IP=$ROS_IP
-#secho 'Exported topside ROS_IP: '$ROS_IP
+#echo 'Exported topside ROS_IP: '$ROS_IP
 
 
 echo 'Exported ASV name: '$ASV_NAME
 echo 'Pinging '$ASV_NAME' ASV'
 ping $ASV_NAME -c 3 ;
-echo 'Pinging topcam at '$TOPCAM_IP 
-ping $TOPCAM_IP -c 3 ;   
+echo 'Pinging topcam at '$PLADYPOS_HIKVISION_IP 
+ping $PLADYPOS_HIKVISION_IP -c 3 ;  
  
 echo 'Launching topside GUI...'
 roslaunch hikvision_ros start_ip_cam_stream_bttm_side_du.launch
