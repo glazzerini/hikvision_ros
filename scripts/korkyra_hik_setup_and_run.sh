797f18be9f619a8082588b8bcb265397b1fe8602
nadir-kapetanovic <nadir.kapetanovic@fer.hr>
2022-04-13 12:40:57 +0200

    Added ASV Korkyra parameters to be exported in a script.

---
+++ scripts/korkyra_hik_setup_and_run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Mljet IP=10.0.15.60
+
+export ASV_NAME=catamaran
+export PLADYPOS_HIKVISION_IP=10.0.108.5
+export PLADYPOS_HIKVISION_STREAM=rtsp://stdops:labos123@$PLADYPOS_HIKVISION_IP:554
+
+echo 'Topside hostname: '$HOSTNAME 
+echo 'Topside ROS distro:'$ROS_DISTRO
+export ROS_MASTER_URI=http://$ASV_NAME:11311/
+echo 'ROS master exported to: '$ROS_MASTER_URI
+#export ROS_IP=$ROS_IP
+#echo 'Exported topside ROS_IP: '$ROS_IP
+
+
+echo 'Exported ASV name: '$ASV_NAME
+echo 'Pinging '$ASV_NAME' ASV'
+ping $ASV_NAME -c 3 ;
+echo 'Pinging topcam at '$PLADYPOS_HIKVISION_IP 
+ping $PLADYPOS_HIKVISION_IP -c 3 ;   
+ 
+echo 'Launching topside GUI...'
+roslaunch hikvision_ros start_ip_cam_stream_bttm_side_du.launch
+
